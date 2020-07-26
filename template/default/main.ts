23e958092560b705f0c2c1d8c3667a4ce4f74f0b
wuchangping <w84814077@163.com>
2020-07-26 14:52:08 +0800

    init

--- template/default/main.ts
+++ template/default/main.ts
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import Filter from './common/Filter'
+import Prototype from './common/Prototype'
 
 Vue.config.productionTip = false
+Filter.init()
+Prototype.init()
 
-new App().$mount()
+uni.onNetworkStatusChange(res => {
+  if(!res.isConnected){
+    // 跳转无网络页面
+    uni.navigateTo({ url: '/subPagesB/noNetWork/index' })
+  }
+})
+
+new App({ store }).$mount()
